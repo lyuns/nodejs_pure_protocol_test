d9d3af586e1e273195c499d8b8c7f78c67daabea
lyuns <lyunst@163.com>
2018-12-10 17:34:29 +0800

    Add session-util relations.

--- http/src/controllers/UserController.js
+++ http/src/controllers/UserController.js
@@ -7,7 +7,6 @@ exports.user = (() => {
             if(!req.session.isLogin){
                 res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
                 res.end(fs.readFileSync(path.resolve(__dirname, '../view/login.html')));
-                return;
             }else{
                 res.setHeader('refresh', '0;url=/');
                 res.writeHead(200, {'Content-Type': 'text/plain;charset=utf-8'});
